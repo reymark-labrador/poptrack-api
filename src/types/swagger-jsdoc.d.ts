c9b1c360cfc7bc5af726c8a0d5beb4d41dc90278
Reymark Labrador <reymark.l@homeofpmg.com>
2025-07-04 04:53:27 +0400

    feat: setup swagger

---
+++ src/types/swagger-jsdoc.d.ts
@@ -0,0 +1,9 @@
+declare module "swagger-jsdoc" {
+  import { OpenAPIObject } from "openapi3-ts"
+  interface Options {
+    definition: OpenAPIObject
+    apis: string[]
+  }
+  function swaggerJSDoc(options: Options): OpenAPIObject
+  export = swaggerJSDoc
+}
